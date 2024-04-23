de48bf83ab740b6bfa1fe54e80f6b333c252f86d
gbayarri <genis.bayarri@irbbarcelona.org>
2024-04-23 11:02:35 +0200

    connection to mongo with & without authentication

--- src/utils/db/index.js
+++ src/utils/db/index.js
@@ -1,5 +1,6 @@
 const { ObjectId } = require('mongodb');
 const pjson = require('../../../package.json');
+const semver = require('semver');
 
 const listAllDocuments = async(db) => {
     // get all documents from the collection
@@ -22,21 +23,6 @@ const loadDocuments = async(docs, db) => {
     // insert documents
     const res = await db.collection('documents').insertMany(docs);
     return res;
-}
-
-// checks mongodb version
-function isGreaterOrEqual(v1, v2) {
-    const [major1, minor1, patch1] = v1.split('.').map(Number);
-    const [major2, minor2, patch2] = v2.split('.').map(Number);
-
-    if (major1 > major2) return true;
-    if (major1 < major2) return false;
-
-    if (minor1 > minor2) return true;
-    if (minor1 < minor2) return false;
-
-    if (patch1 >= patch2) return true;
-    if (patch1 < patch2) return false;
 }
 
 // returns mongo authentication object
@@ -63,7 +49,7 @@ const getMongoAuth = () => {
 
     // check if mongodb version is greater or equal to 4.0.0 and useUnifiedTopology is available
     mongodbVersion = pjson.dependencies.mongodb.replace(/[^0-9.]/g, '');
-    if (!isGreaterOrEqual(mongodbVersion, '4.0.0')) mongoAuth.useUnifiedTopology = true;
+    if (semver.lt(mongodbVersion, '4.0.0')) mongoAuth.useUnifiedTopology = true;
 
     return mongoAuth;
 }
