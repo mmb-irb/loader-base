e9c07b74cef533b6ca4bd56285b1900a8fe37116
gbayarri <genis.bayarri@irbbarcelona.org>
2024-04-23 10:44:11 +0200

    connection to mongo with & without authentication

--- src/commands/list/index.js
+++ src/commands/list/index.js
@@ -16,14 +16,17 @@ const list = async (
     text: ` ${docs.length} documents successfully found:\n`,
   });
 
+  // sort by date in descending order
+  docs.sort((a, b) => new Date(b.created) - new Date(a.created));
+
   const docsTable = docs.map(doc => {
     return {
-      id: doc._id.toString(), // Convert ObjectId to string
-      title: doc.title,
-      description: doc.description,
-      created: doc.created,
+        id: doc._id.toString(), // Convert ObjectId to string
+        title: doc.title,
+        description: doc.description,
+        created: doc.created ? new Date(doc.created).toLocaleString() : '', // Convert Date to human-readable format
     }
-  })
+  });
 
   if(docsTable.length > 0) console.table(docsTable);
 
