2b9c51310e7a9800709f69c0107530a8b99c08b6
gbayarri <genis.bayarri@irbbarcelona.org>
2024-08-14 14:40:15 +0200

    Testing swarm errors

--- src/commands/load/index.js
+++ src/commands/load/index.js
@@ -6,8 +6,6 @@ const { checkJsonFormat } = require('../../utils/json');
 const { insertNewDocument } = require('../../utils/db'); 
 const { getFilesPaths, uploadFiles } = require('../../utils/system/fs');     
 
-const { exec } = require('child_process');
-
 const load = async (
   { file }, { db, bucket }
 ) => {
@@ -43,20 +41,6 @@ const load = async (
 
     return nd
   }
-
-  exec('ls -la /data', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Error executing command: ${error.message}`);
-        return;
-    }
-
-    if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        return;
-    }
-
-    console.log(`stdout: ${stdout}`);
-  });
 
   // check if file exists
   if (fs.existsSync(file)) {
